5ccf97a0584186e4b53a24217d43dcfa83ae064a
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2022-11-16 11:38:42 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', main);
+
+// -----SCENER ATT SKAPA-----
+// DRAGONCASTLE
+// FROZEN LAKE
+// ENCHANTED FOREST
+// SOLEMN VILLAGE
+// CAVE OF MAGIC
+
+// -----FÖREMÅL ATT SKAPA-----
+// DRAGONCRYSTAL - Finns i DRAGONCASTLE, slutmålet
+// SWORD - Finns hos BLACKSMITH i SOLEMN VILLAGE
+// BREAD - Finns hos MERCHANT i SOLEMN VILLAGE
+// MAGIC BOOK - Finns i CAVE OF MAGIC
+// INVISIBILITY CLOAK - Finns hos WIZARD i FROZEN LAKE
+
+// -----KARAKTÄRER ATT SKAPA-----
+// WIZARD
+// BLACKSMITH
+// MERCHANT
+// TROLL
+
+// -----FIENDER ATT SKAPA-----
+// Dragon
+// Evil Wizard
+// Orch
+// Pack of Wolves
