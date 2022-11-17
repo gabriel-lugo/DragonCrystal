a4c2af64ee5cb96dba913d59ae71fe6c68f5e2bc
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2022-11-17 11:29:26 +0100

    added lookaround functions

--- main.js
+++ main.js
@@ -8,75 +8,150 @@ function main() {
 }
 
 
-
-function getPlayerName() {
-    const playerName = "random";
-}
-
-
 // -----SCENES-----
 function loadStartScene() {
 
 }
 
 function loadFrozenLakeScene() {
+    const lookText = document.getElementById('look-text');
+    lookText.innerHTML = "";
+
     const text = document.getElementById('world-text');
+
     const north = document.getElementById('north');
     const south = document.getElementById('south');
-    const east = document.getElementById('east');
     const west = document.getElementById('west');
-    text.textContent = "You are standing by the Frozen Lake. You see a Wizard.";
-    north.addEventListener('click', loadSolemnVillageScene);
+    const east = document.getElementById('east');
 
+    const look = document.getElementById('look-around');
+
+
+
+    text.textContent = "You are standing by the Frozen Lake.";
+    north.addEventListener('click', loadSolemnVillageScene);
+    south.addEventListener('click', loadFrozenLakeScene);
+    west.addEventListener('click', loadFrozenLakeScene);
+    east.addEventListener('click', loadFrozenLakeScene);
+    look.addEventListener('click', lookingAroundFrozenLake);
 }
 
 function loadSolemnVillageScene() {
+    const lookText = document.getElementById('look-text');
+    lookText.innerHTML = "";
     const text = document.getElementById('world-text');
     const north = document.getElementById('north');
     const south = document.getElementById('south');
-    const east = document.getElementById('east');
     const west = document.getElementById('west');
+    const east = document.getElementById('east');
+
+    const look = document.getElementById('look-around');
+
+
     text.textContent = "You are in the Solemn Village.";
     north.addEventListener('click', loadEnchantedForestScene);
     south.addEventListener('click', loadFrozenLakeScene);
-
+    west.addEventListener('click', loadSolemnVillageScene);
+    east.addEventListener('click', loadSolemnVillageScene);
+    look.addEventListener('click', lookingAroundSolemnVillage);
 }
 
 function loadEnchantedForestScene() {
+    const lookText = document.getElementById('look-text');
+    lookText.innerHTML = "";
     const text = document.getElementById('world-text');
     const north = document.getElementById('north');
     const south = document.getElementById('south');
-    const east = document.getElementById('east');
     const west = document.getElementById('west');
+    const east = document.getElementById('east');
+
+    const look = document.getElementById('look-around');
+
+
     text.textContent = "You are in the Enchanted Forest";
     north.addEventListener('click', loadDragonCastleScene);
     south.addEventListener('click', loadSolemnVillageScene);
     west.addEventListener('click', loadCaveOfMagicScene);
-
+    east.addEventListener('click', loadEnchantedForestScene);
+    look.addEventListener('click', lookingAroundEnchantedForest);
 }
 
 function loadCaveOfMagicScene() {
+    const lookText = document.getElementById('look-text');
+    lookText.innerHTML = "";
     const text = document.getElementById('world-text');
     const north = document.getElementById('north');
     const south = document.getElementById('south');
-    const east = document.getElementById('east');
     const west = document.getElementById('west');
-    text.textContent = "You are in the Cave of Magic";
-    east.addEventListener('click', loadEnchantedForestScene);
+    const east = document.getElementById('east');
 
+    const look = document.getElementById('look-around');
+
+
+    text.textContent = "You are in the Cave of Magic";
+    north.addEventListener('click', loadCaveOfMagicScene);
+    south.addEventListener('click', loadCaveOfMagicScene);
+    west.addEventListener('click', loadCaveOfMagicScene);
+    east.addEventListener('click', loadEnchantedForestScene);
+    look.addEventListener('click', lookingAroundCaveOfMagic);
 }
 
 function loadDragonCastleScene() {
+    const lookText = document.getElementById('look-text');
+    lookText.innerHTML = "";
     const text = document.getElementById('world-text');
     const north = document.getElementById('north');
     const south = document.getElementById('south');
-    const east = document.getElementById('east');
     const west = document.getElementById('west');
+    const east = document.getElementById('east');
+
+    const look = document.getElementById('look-around');
+
     text.textContent = "You are outside the DragonCastle";
+    north.addEventListener('click', loadDragonCastleScene);
     south.addEventListener('click', loadEnchantedForestScene);
+    west.addEventListener('click', loadDragonCastleScene);
+    east.addEventListener('click', loadDragonCastleScene);
+    look.addEventListener('click', lookingAroundDragonCastle);
+}
+
+// -----LOOKING AROUND-----
+function lookingAroundFrozenLake() {
+    const lookText = document.getElementById('look-text');
+    lookText.innerHTML = "";
+
+    lookText.textContent = "You see a Wizard.";
 
 }
 
+function lookingAroundSolemnVillage() {
+    const lookText = document.getElementById('look-text');
+    lookText.innerHTML = "";
+
+    lookText.textContent = "You see a Blacksmith and a Merchant.";
+
+}
+
+function lookingAroundEnchantedForest() {
+    const lookText = document.getElementById('look-text');
+    lookText.innerHTML = "";
+
+    lookText.textContent = "You see some animals."
+}
+
+function lookingAroundCaveOfMagic() {
+    const lookText = document.getElementById('look-text');
+    lookText.innerHTML = "";
+
+    lookText.textContent = "You see a TROLL guarding the entrance.";
+}
+
+function lookingAroundDragonCastle() {
+    const lookText = document.getElementById('look-text');
+    lookText.innerHTML = "";
+
+    lookText.textContent = "You see two ORCHS guarding the entrance.";
+}
 
 
 // -----SCENER ATT SKAPA-----
